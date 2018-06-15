4d0bd8d27f1dd8f7b5d410d668b1a5f899a127cd
Ihor Yemets <i.yemets@p1k.co.uk>
2018-06-15 11:24:31 +0300

    Add requestIdleCallback polyfill

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+export type IdleDeadline = Readonly<{
+    didTimeout: boolean;
+    timeRemaining: () => number;
+}>;
+
+export type IdleCallback = (deadline: IdleDeadline) => void;
+export type AnyFunction = (...args: any[]) => void;
+
+export type RequestIdleCallbackOptions = {
+    timeout?: number;
+};
+export type RequestIdleCallback = (fn: IdleCallback | AnyFunction, options?: RequestIdleCallbackOptions) => number;
+export type CancelIdleCallback = (id: number) => void;
+
+const ctx: any = window || global;
+
+export const requestIdleCallback: RequestIdleCallback = ctx.requestIdleCallback || requestIdleCallbackShim;
+export const cancelIdleCallback: (id: number) => void = ctx.cancelIdleCallback || ctx.clearTimeout;
+
+function requestIdleCallbackShim(callback: IdleCallback): number {
+    const start = Date.now();
+    return ctx.setTimeout(() => {
+        callback({
+            didTimeout: false,
+            timeRemaining: () => Math.max(0, 12 - (Date.now() - start)),
+        });
+    });
+}
